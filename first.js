fde4eee9d8921b638bd8fbceb77d1410b676bb86
SanskarDhyani98 <sanskardhyani98@gmail.com>
2024-01-01 21:53:43 +0530

    Start

---
+++ first.js
@@ -0,0 +1,15 @@
+// console.log("Apna College");
+let student = {
+  name: "sanskar",
+  cgpa: 9.3,
+  rollno: 22103133,
+  semester: 4,
+  internhip: false,
+};
+for (let key in student) {
+  console.log(student.key);
+}
+// let str = "sanskar";
+// for (let i of str) console.log(i);
+let num = prompt("Enter no.");
+console.log(num);
